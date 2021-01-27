1bce669823fb0699fee6e6d993a3935b8a666e83
Valentin Barit <Valentin.Barit@cdiscount.com>
2021-01-27 14:47:16 +0100

    test: 💍 create naive tests

---
+++ fizzbuzz.js
@@ -0,0 +1,8 @@
+function fizzbuzz(number) {
+  if (number % 3 === 0 && number % 5 === 0) return "fizzbuzz";
+  if (number % 3 === 0) return "fizz";
+  if (number % 5 === 0) return "buzz";
+  return number.toString();
+}
+
+module.exports = fizzbuzz;
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,1 +1,19 @@
-describe("fizzbuzz should,",() =>)
+const fizzbuzz = require("./fizzbuzz");
+
+describe("fizzbuzz should,", () => {
+  it("returns a regular number when is regular", () => {
+    expect(fizzbuzz(1)).toBe("1");
+  });
+
+  it("returns 'fizz' when number is divisible by 3", () => {
+    expect(fizzbuzz(3)).toBe("fizz");
+  });
+
+  it("returns 'buzz' when number is divisible by 5", () => {
+    expect(fizzbuzz(5)).toBe("buzz");
+  });
+
+  it("returns 'fizzbuzz' when number is divisible by 5 and 3", () => {
+    expect(fizzbuzz(15)).toBe("fizzbuzz");
+  });
+});
